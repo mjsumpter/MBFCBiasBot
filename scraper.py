5cb457be32f6795e9be4616b7f1e2ab6d48d8cf3
mjsumpter <mjsumpter@gmail.com>
2019-02-14 21:25:40 -0500

    implement regex for finding link and factual_rating on each bias page

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 import sqlite3
+import re
 from sqlite3 import Error
 from bs4 import BeautifulSoup
 
@@ -26,8 +27,8 @@ def create_source(conn, source):
     :param source:
     :return: source id
     """
-    sql = ''' INSERT OR IGNORE INTO sources(source_title,href,bias_label,factual_rating,bias_desc,source_desc)
-              VALUES(?,?,?,?,?,?) '''
+    sql = ''' INSERT OR IGNORE INTO sources(source_title,href,link,bias_label,factual_rating,bias_desc,source_desc)
+              VALUES(?,?,?,?,?,?,?) '''
     cur = conn.cursor()
     cur.execute(sql, source)
     return cur.lastrowid
@@ -48,18 +49,28 @@ def main():
             bias_soup = BeautifulSoup(r_bias.content, 'html.parser')
 
             for a in bias_soup.select('.entry p a'):
+                # get source title from sources list
                 source_title = a.text.strip()
+                # href is link to full info page on source
                 href = a['href']
 
                 r_source = requests.get(href)
                 source_soup = BeautifulSoup(r_source.content, 'html.parser')
 
+                # link is link to news site
+                pattern = re.compile(r"(?:^|\W)Source:(?:$|\W)")
+                index = pattern.search(source_soup.text).start()
+                link = source_soup.text[index+9:index+100]
+                link = link[:link.find('\n')]
+
                 html = source_soup.select_one('.entry-header > h1')
                 bias_label = html.text.strip() if html else "Error"
 
-                html = source_soup.select_one(
-                    '.entry-content p:nth-child(2) > span > strong')
-                factual_rating = html.text.strip() if html else "Error"
+                pattern = re.compile(r"(?:^|\W)Reporting:(?:$|\W)")
+                index = pattern.search(source_soup.text).start()
+                factual_rating = source_soup.text[index+12:index+20]
+                factual_rating = factual_rating[:factual_rating.find('\n')]
+                factual_rating = factual_rating if factual_rating else "Error"
 
                 html = source_soup.select_one('.entry-content p:nth-child(1)')
                 bias_desc = html.text.strip().replace(u'\xa0', u' ') if html else "Error"
@@ -67,10 +78,10 @@ def main():
                 html = source_soup.select_one('.entry-content p:nth-child(3)')
                 source_desc = html.text.strip().replace(u'\xa0', u' ') if html else "Error"
 
-                source = (source_title, href, bias_label,
+                source = (source_title, href, link, bias_label,
                           factual_rating, bias_desc, source_desc)
                 create_source(conn, source)
-                print("Added source " + str(i))
+                print("Added " + bias + " source " + str(i))
                 i += 1
 
 
