4ea929cb27d320b2c2ffbaa52a1efa6c02ccd1cf
mjsumpter <mjsumpter@gmail.com>
2019-02-16 23:52:15 -0500

    added arrow templates

--- helper.py
+++ helper.py
@@ -66,7 +66,40 @@ def create_source(conn, source):
     return cur.lastrowid
 
 
-def build_message(source):
+def source_to_dict(source):
+    source_dict["title"] = source[0]
+    source_dict["href"] = source[1]
+    source_dict["link"] = source[2]
+    source_dict["bias"] = source[3]
+    source_dict["rating"] = source[4]
+    source_dict["desc1"] = source[5]
+    source_dict["desc2"] = source[6]
+
+    if source_dict["bias"] == "LEFT BIAS":
+        source_dict["bias_arrow"] = "<==O == == == == == == =|= == == == == == == ==>\n\n"
+    elif source_dict["bias"] == "LEFT-CENTER BIAS":
+        source_dict["bias_arrow"] = "<== == == == ==O == == =|= == == == == == == ==>\n\n"
+    elif source_dict["bias"] == "LEAST BIASED":
+        source_dict["bias_arrow"] = "<== == == == == == == =(|)= == == == == == == ==>\n\n"
+    elif source_dict["bias"] == "RIGHT-CENTER BIAS":
+        source_dict["bias_arrow"] = "<== == == == == == == =|= == == O== == == == ==>\n\n"
+    elif source_dict["bias"] == "RIGHT BIAS":
+        source_dict["bias_arrow"] = "<== == == == == == == =|= == == == == == == O==>\n\n"
+    elif source_dict["bias"] == "PRO-SCIENCE":
+        source_dict["bias_arrow"] = "<11 00 11 11 00 01 00 =|= 11 00 10 01 00 00 10>\n\n"
+    elif source_dict["bias"] == "CONSPIRACY-PSEUDOSCIENCE":
+        source_dict["bias_arrow"] = "< o_O o_O o_O o_O o_O =|= o_O o_O o_O o_O o_O >\n\n"
+    elif source_dict["bias"] == "QUESTIONABLE SOURCE":
+        source_dict["bias_arrow"] = "<(>_<)(>_<)(>_<)(>_<)=|=(>_<)(>_<)(>_<)(>_<)(>_<)>\n\n"
+    elif source_dict["bias"] == "SATIRE":
+        source_dict["bias_arrow"] = "<(⌒▽⌒）(⌒▽⌒）(⌒▽⌒）(⌒▽⌒）=|=(⌒▽⌒）(⌒▽⌒）(⌒▽⌒）(⌒▽⌒）>\n\n"
+    else:
+        source_dict["bias_arrow"] = ""
+
+    return source_to_dict
+
+
+def build_message(source_dict):
     template = (
         "**{bias}**\n\n"
         "{arrow}<==O == == == == == == |= == == == == == == == >\n\n"
@@ -77,3 +110,5 @@ def build_message(source):
         "[MediaBiasFactCheck](https: // mediabiasfactcheck.com/about/) | [Code/Docs](https: // github.com/mjsumpter/MBFCBiasBot) | [Feedback](http: // np.reddit.com/message/compose /?to=MBFCBiasBot & subject=Feedback)\n"
         "- -- | ---- | ----"
     )
+
+    return template.format(**source_dict)
